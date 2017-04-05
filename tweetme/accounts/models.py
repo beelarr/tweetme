a7be7696cd6a443d6ec99638b9366441753dab3c
beelarr <bryonl@me.com>
2017-04-05 11:02:23 -0500

    Added models.py in accounts to add following and followed by functionality 47

--- tweetme/accounts/models.py
+++ tweetme/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# Create your models he
+
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+	following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+
+	def __str__(self):
+		return str(self.following.all().count())
