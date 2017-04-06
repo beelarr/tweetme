f21ac95d5f6d43f8eddee7857bb9bc1eab156cec
beelarr <bryonl@me.com>
2017-04-06 17:05:53 -0500

    Added toggle to Follow/Unfollow buttons 49 & 50

--- tweetme/accounts/urls.py
+++ tweetme/accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
@@ -8,6 +8,7 @@ urlpatterns = [
 	# url(r'^$', TweetListAPIView.as_view(), name='list'),
 	# url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
 	url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+	url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
 	# url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
 	# url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete')
 
