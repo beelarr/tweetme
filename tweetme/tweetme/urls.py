da55cb54d73c03a61176f0310a16183a845c9f4c
beelarr <bryonl@me.com>
2017-04-11 12:06:25 -0500

    Added API for User tweet detail page and reworked the API on the base.html. Also worked styling on detail and tweet_list.html

--- tweetme/tweetme/urls.py
+++ tweetme/tweetme/urls.py
@@ -15,22 +15,23 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
-from tweets.views import TweetListView
+
 from hashtags.views import HashTagView
-
-
+from tweets.views import TweetListView
+from .views import home
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'^$', TweetListView.as_view(), name='home'),
-	url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name='hashtag'),
-	url(r'^tweet/', include('tweets.urls', namespace='tweet')),
-	url(r'', include('accounts.urls', namespace='profiles')),
-	url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
+    url(r'^admin/', admin.site.urls), #admin/
+    url(r'^$', TweetListView.as_view(), name='home'), #/
+    url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name='hashtag'),
+    url(r'^tweet/', include('tweets.urls', namespace='tweet')),
 
+    url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
+    url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),
+    url(r'^', include('accounts.urls', namespace='profiles')),
 
 ]
 
